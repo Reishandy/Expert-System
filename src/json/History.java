7c01622dcd79d040412468c5a240fe2a89e22b22
Reishandy <isthisruxury@gmail.com>
2023-04-15 12:39:33 +0700

    change history to current time

--- src/json/History.java
+++ src/json/History.java
@@ -2,27 +2,31 @@ package json;
 
 import answer.Answer;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 
 public class History {
-    private HashMap<Integer, ArrayList<Answer>> history;
+    private HashMap<String, ArrayList<Answer>> history;
 
     public History() {
         this.history = new HashMap<>();
     }
 
     public void addHistory(ArrayList<Answer> answers) {
-        int count = history.size();
-        history.put(count + 1, answers);
+        history.put(
+                new SimpleDateFormat("HH:mm:ss | dd/MM/yyyy")
+                        .format(Calendar.getInstance().getTime()),
+                answers);
     }
 
-    public void addHistory(HashMap<Integer, ArrayList<Answer>> history) {
+    public void addHistory(HashMap<String, ArrayList<Answer>> history) {
         if (history == null) return;
         this.history = history;
     }
 
-    public HashMap<Integer, ArrayList<Answer>> getHistory() {
+    public HashMap<String, ArrayList<Answer>> getHistory() {
         return history;
     }
 
